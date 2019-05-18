346ee29a3634e3f4d6d3be864d4d1d014292bd31
Susumu Ishizuka <susumu.ishizuka@kii.com>
2019-05-18 10:38:55 +0900

    fixed some bugs.
    output doesn't match to the original C++ code due to the different order
    of eplison net set.

--- solovay_kitaev/solovay_kitaev.py
+++ solovay_kitaev/solovay_kitaev.py
@@ -9,10 +9,21 @@ EPS = 1e-12
 SQRT2 = 2 ** 0.5
 ALPHA = (2 + SQRT2) ** 0.5 / 2
 BETA = (2 - SQRT2) ** 0.5 / 2
-NUM_CLIFFORD = 24
-GATESTR = ["I","X","(I+iX)","(I-iX)","(I+iY)","(I-iY)","Z","(I+iZ)","(I-iZ)","T"]
-DAG_ID = [ 0, 1, 3, 2, 5, 4, 6, 8, 7, -1 ]
-TID = 9
+GATESTR = ("I","X","(I+iX)","(I-iX)","(I+iY)","(I-iY)","Z","(I+iZ)","(I-iZ)","T", "H")
+DAGGERS = {
+    "I": ("I",),
+    "X": ("X",),
+    "Y": ("Y",),
+    "Z": ("Z",),
+    "(I+iX)": ("(I-iX)",),
+    "(I-iX)": ("(I+iX)",),
+    "(I+iY)": ("(I-iY)",),
+    "(I-iY)": ("(I+iY)",),
+    "(I+iZ)": ("(I-iZ)",),
+    "(I-iZ)": ("(I+iZ)",),
+    "T": ("X", "T", "X"),
+    "H": ("H",),
+}
 
 # allowed number of T-gate in epsilon net
 MAX_HIERARCHY = 3
@@ -25,7 +36,8 @@ class Uop:
         self.hierarchy = hierarchy
         self.construction = construction
         self.v = (i, x, y, z)
-        self._fixup_direction()
+        if self._needs_fixup_direction():
+            self.v = tuple((-value for value in self.v))
         if normalize:
             norm = sum([v ** 2 for v in self.v]) ** 0.5
             self.v = tuple((v / norm for v in self.v))
@@ -34,16 +46,18 @@ class Uop:
             assert 1 -EPS < sum((v**2 for v in self.v)) < 1 + EPS
         assert type(self.construction) == list
         for c in self.construction:
-            assert type(c) == int, "bad construction: %s" % self.construction
+            assert c in GATESTR, "bad construction: %s" % self.construction
 
-    def _fixup_direction(self):
+    def _needs_fixup_direction(self):
         """to omit double-counting in SU(2), every variable is inverted if the first non-zero value is negative"""
         for v in self.v:
-            if v < EPS:
+            if -EPS < v < EPS:
                 continue
             if v < 0:
-                self.v = tuple((-value for value in self.v))
-                break
+                return True
+            else:
+                return False
+        return False
 
     @staticmethod
     def from_matrix(matrix, hierarchy=0, construction=[], normalize=True):
@@ -53,9 +67,7 @@ class Uop:
         y = (matrix[0][1] - matrix[1][0]) / 2
         z = (matrix[0][0] - matrix[1][1]) / 2j
         # fix phase
-        print((i, x, y, z))
         i, x, y, z = Uop._fix_phase((i, x, y, z))
-        print((i, x, y, z))
 
         return Uop(i, x, y, z, hierarchy, construction, True)
 
@@ -106,12 +118,8 @@ class Uop:
 
     def dagger(self):
         ncon = []
-        for c in self.construction:
-            if c == TID:
-                # T^dag = XTX
-                ncon += [1, TID, 1]
-            else:
-                ncon.append(DAG_ID[c])
+        for c in reversed(self.construction):
+            ncon += DAGGERS[c]
         return Uop(self.i, -self.x, -self.y, -self.z, self.hierarchy, ncon)
 
     def __str__(self):
@@ -119,41 +127,28 @@ class Uop:
         return ("****\n"
                 "#T gate : {self.hierarchy}\n"
                 "{self.v[0]:f} I + {self.v[1]:f} iX + {self.v[2]:f} iY + {self.v[3]:f} iZ\n"
-                "rot = {rot:f}π, axis = ({axis})\n"
+                "rot = {rot:f}π, axis = {axis}\n"
                 "****".format(self=self,
                               rot= 2 * math.acos(self.v[0])/math.pi,
-                              axis=tuple((v/nn for v in self.v[1:]))))
-
-    def set_clifford(self):
-        """ create length24 list of [(u*C) for C in Clifford]
-        Clifford can be constructed with product((I,iX,I+iX,I-iX,I+iY,I-iY) , (I,iZ,I+iZ,I-iZ))"""
-        i, x, y, z = self.v
-        result = []
-        result.append(self.clone()) # I
-        result.append(Uop(-x, i, -z, y, self.hierarchy, self.construction + [1])) # iX
-        result.append(Uop((i-x)/SQRT2, (x+i)/SQRT2, (y-z)/SQRT2, (z+y)/SQRT2, self.hierarchy, self.construction + [2])) # I+iX
-        result.append(Uop((i+x)/SQRT2, (x-i)/SQRT2, (y+z)/SQRT2, (z-y)/SQRT2, self.hierarchy, self.construction + [3])) # I-iX
-        result.append(Uop((i-y)/SQRT2, (x+z)/SQRT2, (y+i)/SQRT2, (z-x)/SQRT2, self.hierarchy, self.construction + [4])) # I+iY
-        result.append(Uop((i+y)/SQRT2, (x-z)/SQRT2, (y-i)/SQRT2, (z+x)/SQRT2, self.hierarchy, self.construction + [5])) # I-iY
-        for idx in range(6):
-            i, x, y, z = result[idx].v
-            c = [idx] if idx != 0 else []
-            result.append(Uop(-z, -y, x, i, self.hierarchy, self.construction + c + [6]))
-            result.append(Uop((i-z)/SQRT2, (x-y)/SQRT2, (y+x)/SQRT2, (z+i)/SQRT2, self.hierarchy, self.construction + c + [7]))
-            result.append(Uop((i+z)/SQRT2, (x+y)/SQRT2, (y-x)/SQRT2, (z-i)/SQRT2, self.hierarchy, self.construction + c + [8]))
-
-        return result
+                              axis=tuple((v/nn if nn > 0 else 0 for v in self.v[1:]))))
 
     def __hash__(self):
         return hash((self.v, self.hierarchy, tuple(self.construction)))
         
     def construction_str(self):
-        return "".join([GATESTR[c] for c in self.construction])
+        return "".join(self.construction)
 
     def operator_distance(self, other):
+        def max_eigen_value(mat):
+            p = abs(mat[0][0])**2 + abs(mat[1][0])**2
+            q = abs(mat[0][1])**2 + abs(mat[1][1])**2
+            r2 = abs(mat[0][0] * mat[0][1].conjugate() + mat[1][0].conjugate() * mat[1][1]) ** 2
+            return (p + q + ((p - q)** 2 + 4 * r2) ** 0.5)/2
         u = self.matrix_form()
         v = other.matrix_form()
-        values = np.linalg.eigvalsh(u-v)
+        diff = u - v
+        values = [max_eigen_value(diff)]
+        assert values[-1] >= 0, str(self)
         return values[-1] ** 0.5
 
     def get_similar(self, uop_list):
@@ -174,13 +169,38 @@ class Uop:
          and self.construction == self.construction
          and self.hierarchy == self.hierarchy)
 
+    def is_similar(self, other):
+        for left, right in zip(self.v, other.v):
+            if not (left - EPS < right < left + EPS):
+                return False
+        return True
 
 
 I = Uop(1, 0, 0, 0, 0, [])
 
+def clifford_set(u):
+    """ create length24 list of [(u*C) for C in Clifford]
+    Clifford can be constructed with product((I,iX,I+iX,I-iX,I+iY,I-iY) , (I,iZ,I+iZ,I-iZ))"""
+    i, x, y, z = u.v
+    result = []
+    result.append(u.clone()) # I
+    result.append(Uop(-x, i, -z, y, u.hierarchy, u.construction + ["X"])) # iX, but treat it as X due to only phase difference
+    result.append(Uop((i-x)/SQRT2, (x+i)/SQRT2, (y-z)/SQRT2, (z+y)/SQRT2, u.hierarchy, u.construction + ["(I+iX)"]))
+    result.append(Uop((i+x)/SQRT2, (x-i)/SQRT2, (y+z)/SQRT2, (z-y)/SQRT2, u.hierarchy, u.construction + ["(I-iX)"]))
+    result.append(Uop((i-y)/SQRT2, (x+z)/SQRT2, (y+i)/SQRT2, (z-x)/SQRT2, u.hierarchy, u.construction + ["(I+iY)"]))
+    result.append(Uop((i+y)/SQRT2, (x-z)/SQRT2, (y-i)/SQRT2, (z+x)/SQRT2, u.hierarchy, u.construction + ["(I-iY)"]))
+    for idx in range(6):
+        i, x, y, z = result[idx].v
+        c = result[idx].construction[-1:] if idx != 0 else []
+        result.append(Uop(-z, -y, x, i, u.hierarchy, u.construction + c + ["Z"])) # iZ
+        result.append(Uop((i-z)/SQRT2, (x-y)/SQRT2, (y+x)/SQRT2, (z+i)/SQRT2, u.hierarchy, u.construction + c + ["(I+iZ)"]))
+        result.append(Uop((i+z)/SQRT2, (x+y)/SQRT2, (y-x)/SQRT2, (z-i)/SQRT2, u.hierarchy, u.construction + c + ["(I-iZ)"]))
 
-def generate_epsilon_network():
-    result = set(I.set_clifford())
+    return result
+
+
+def generate_epsilon_network(max_hierarchy):
+    result = clifford_set(I)
     queue = collections.deque(result)
     current_hierarchy = 0
 
@@ -192,15 +212,16 @@ def generate_epsilon_network():
         ty = y*ALPHA + x*BETA
         tz = z*ALPHA + i*BETA
 
-        circuits = Uop(ti, tx, ty, tz, u.hierarchy + 1, u.construction + [TID]).set_clifford()
+        circuits = clifford_set(Uop(ti, tx, ty, tz, u.hierarchy + 1, u.construction + ["T"]))
         if u.hierarchy + 1 > current_hierarchy:
             current_hierarchy += 1
 
         for c in circuits:
-            if c in result:
-                break
-            if c.hierarchy < MAX_HIERARCHY:
-                result.add(c)
+            if any((c.is_similar(o) for o in result)):
+                continue
+            result.append(c)
+            if c.hierarchy < max_hierarchy:
+                queue.append(c)
     return result
 
 
--- test/test_sk.py
+++ test/test_sk.py
@@ -3,31 +3,71 @@ import math
 import pytest
 from math import sin, cos, pi, e
 
+Uop = sk.Uop
+SQRT2 = sk.SQRT2
+ERR = 10**-5
 
-#@pytest.mark.parametrize("count, expected",
-        #[1, "(I-iZ)T"])
-def test():
-    #t = sk.Uop(0, 1, 0, 0, 0, [])
+oI = Uop(1, 0, 0, 0, 0)
+oX = Uop(0, 1, 0, 0, 0, ['X'])
+osX = Uop(1 / SQRT2, 1 / SQRT2, 0, 0, 0, ['(I+iX)'])
+osXd = Uop(1 / SQRT2, -1 / SQRT2, 0, 0, 0, ['(I-iX)'])
+osY = Uop(1 / SQRT2, 0, 1 / SQRT2, 0, 0, ['(I+iY)'])
+osYd = Uop(1 / SQRT2, 0, -1 / SQRT2, 0, 0, ['(I-iY)'])
+oZ = Uop(0, 0, 0, 1, 0, ['Z'])
+osZ = Uop(1 / SQRT2, 0, 0, 1 / SQRT2, 0, ['(I+iZ)'])
+osZd = Uop(1 / SQRT2, 0, 0, -1 / SQRT2, 0, ['(I-iZ)'])
+T = Uop(cos(pi/8.), 0, 0, sin(pi/8.), 0, ['T'])
+gates = {"I": oI,
+        "X": oX,
+        "(I+iX)": osX,
+        "(I-iX)": osXd,
+        "(I+iY)": osY,
+        "(I-iY)": osYd,
+        "Z": oZ,
+        "(I+iZ)": osZ,
+        "(I-iZ)": osZd,
+        "T": T}
+
+
+@pytest.mark.parametrize("left, right, expected",(
+        [oI, oZ, SQRT2],
+        [oZ, oI, SQRT2],
+        [oI, osX, 1],
+        [oZ, osX, math.sqrt(3)],
+        [oZ, oZ, 0],
+))
+def test_distance(left, right, expected):
+    assert expected - ERR < left.operator_distance(right) < expected + ERR
+    
+def verify_construction(u):
+    from_construction = []
+    from_construction.append(oI)
+    for c in u.construction:
+        op = from_construction[-1] @ gates[c]
+        from_construction.append(op)
+    assert u.construction == from_construction[-1].construction
+    assert u.is_similar(from_construction[-1]), "approximate: {}, from_construction: {}".format(u, from_construction[-1])
+
+@pytest.mark.parametrize("count, expected",(
+        [0, "T"],
+        [1, "(I+iY)T(I-iY)(I+iX)T(I-iY)(I-iZ)(I+iY)XTX(I-iY)(I+iZ)(I+iY)XTX(I-iX)T"],
+))
+def test(count, expected):
     t = sk.Uop(math.cos(math.pi / 16), 0, 0, math.sin(math.pi / 16))
     print(t)
-    s = sk.generate_epsilon_network()
-    for i in range(5):
-        print(i)
-        result = sk.solovay_kitaev(s, t, i)
-        print(result.construction_str())
-        print("Uop: {}".format(result))
-        print("diff: {}".format(result.operator_distance(t)))
-    assert False
+    s = sk.generate_epsilon_network(3)
+    result = sk.solovay_kitaev(s, t, count)
+    print(result)
+    print("dist: {}".format(result.operator_distance(t)))
+    assert expected == result.construction_str()
 
-ERR = 10**-5
-SQRT2 = sk.SQRT2
 
 @pytest.mark.parametrize("matrix, expected",[
-        ([[1,0],[0,1]], (1, 0, 0, 0)),  # I
-        ([[0,1],[1,0]], (0, 1, 0, 0)),  # X
-        ([[0,-1j],[1j,0]], (0, 0, 1, 0)),  # Y
-        ([[1,0],[0,-1]], (0, 0, 0, 1)),  # Z
-        ([[1,1],[1,-1]], (0, 1/SQRT2, 0, 1/SQRT2)),  # Hadamard
+    ([[1,0],[0,1]], (1, 0, 0, 0)),  # I
+    ([[0,1],[1,0]], (0, 1, 0, 0)),  # X
+    ([[0,-1j],[1j,0]], (0, 0, 1, 0)),  # Y
+    ([[1,0],[0,-1]], (0, 0, 0, 1)),  # Z
+    ([[1,1],[1,-1]], (0, 1/SQRT2, 0, 1/SQRT2)),  # Hadamard
 ])
 def test_from_matrix(matrix, expected):
     # Hadamard -> 
@@ -36,11 +76,11 @@ def test_from_matrix(matrix, expected):
         assert expected[i] - ERR < u.v[i] < expected[i] + ERR
 
 @pytest.mark.parametrize("args",[
-        (1, 0, 0, 0),  # I
-        (0, 1, 0, 0),  # X
-        (0, 0, 1, 0),  # Y
-        (0, 0, 0, 1),  # Z
-        (0, 1/SQRT2, 0, 1/SQRT2),  # Hadamard
+    (1, 0, 0, 0),  # I
+    (0, 1, 0, 0),  # X
+    (0, 0, 1, 0),  # Y
+    (0, 0, 0, 1),  # Z
+    (0, 1/SQRT2, 0, 1/SQRT2),  # Hadamard
 ])
 def test_matrix_form(args):
     # Hadamard -> 
